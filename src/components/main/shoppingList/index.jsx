579394081e6c4dd5260c9e490adade3fcdc1dd56
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-10-21 14:57:33 +0300

    disable shoppinglist animation

--- src/components/main/shoppingList/index.jsx
+++ src/components/main/shoppingList/index.jsx
@@ -47,55 +47,78 @@ const SoppingList = props => {
   return (
     <section className={styles.shopping}>
       { boovatech.shoppingList ? (
-        <div>
-          <span>{boovatech.shoppingList.date || (new Date()).getDateString()}</span>
-          <nav>
-            <input onChange={actions.addShoppingItem} value={boovatech.itemToAdd || ''} />
-            <FloatingActionButton
-              mini
-              onClick={actions.addItem}
-              disabled={!(boovatech.active && boovatech.active.add)}
-            >
-              <ContentAdd />
-            </FloatingActionButton>
-          </nav>
-          <section>
-            <ReactCSSTransitionGroup
-              transitionName={{
-                enter: animation.enter,
-                enterActive: animation.enterActive,
-                leave: animation.leave,
-                leaveActive: animation.leaveActive,
-                appear: animation.appear,
-                appearActive: animation.appearActive
-              }}
-              transitionAppear={false}
-              transitionEnter
-              transitionLeave
-              transitionEnterTimeout={500}
-              transitionLeaveTimeout={100}
-            >
-              {boovatech.shoppingList.shoppingList && boovatech.shoppingList.shoppingList.slice ?
-                boovatech.shoppingList.shoppingList.map(item =>
-                  <ShoppingItem key={item.id} item={item} actions={actions} />
-                  )
-                :
-                null
-              }
-            </ReactCSSTransitionGroup>
-          </section>
-          <footer>
-            <FloatingActionButton
-              mini
-              secondary
-              onClick={() => console.log('click')}
-              disabled={!(boovatech.active && boovatech.active.save)}
-              onClick={() => actions.saveList(user)}
-            >
-              <ContentSave />
-            </FloatingActionButton>
-          </footer>
-        </div>
+        <ReactCSSTransitionGroup
+          transitionName={{
+            enter: animation.enter,
+            enterActive: animation.enterActive,
+            leave: animation.leave,
+            leaveActive: animation.leaveActive,
+            appear: animation.appear,
+            appearActive: animation.appearActive
+          }}
+          transitionAppear
+          transitionEnter
+          transitionLeave
+          transitionAppearTimeout={800}
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={100}
+        >
+          <div>
+            <span>{boovatech.shoppingList.date || (new Date()).getDateString()}</span>
+            <nav>
+              <input onChange={actions.addShoppingItem} value={boovatech.itemToAdd || ''} />
+              <FloatingActionButton
+                mini
+                onClick={actions.addItem}
+                disabled={!(boovatech.active && boovatech.active.add)}
+              >
+                <ContentAdd />
+              </FloatingActionButton>
+            </nav>
+            <section>
+              {/*
+              <ReactCSSTransitionGroup
+                transitionName={{
+                  enter: animation.enter,
+                  enterActive: animation.enterActive,
+                  leave: animation.leave,
+                  leaveActive: animation.leaveActive,
+                  appear: animation.appear,
+                  appearActive: animation.appearActive
+                }}
+                transitionAppear={false}
+                transitionEnter
+                transitionLeave
+                transitionEnterTimeout={300}
+                transitionLeaveTimeout={100}
+              >
+              */}
+              <span>
+                {boovatech.shoppingList.shoppingList && boovatech.shoppingList.shoppingList.slice ?
+                  boovatech.shoppingList.shoppingList.map(item =>
+                    <ShoppingItem key={item.id} item={item} actions={actions} />
+                    )
+                  :
+                  null
+                }
+              </span>
+              {/*
+              </ReactCSSTransitionGroup>
+              */}
+            </section>
+            <footer>
+              <FloatingActionButton
+                mini
+                secondary
+                onClick={() => console.log('click')}
+                disabled={!(boovatech.active && boovatech.active.save)}
+                onClick={() => actions.saveList(user)}
+              >
+                <ContentSave />
+              </FloatingActionButton>
+            </footer>
+          </div>
+        </ReactCSSTransitionGroup>
         ) : null}
     </section>
   );
